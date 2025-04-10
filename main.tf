577b8df83e9025bd8141dcb8a1c8639d8a22a7ff
ronierisonmaciel <ronierison.maciel@gmail.com>
2025-04-10 11:12:07 -0300

    :alien::fire: Add initial project files for Docker and Terraform deployment

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-academia-cloud"
+  location = "East US"
+}
+
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "app-service-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "nginx-webapp-cloud"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+
+  site_config {
+    application_stack {
+      docker_image_name = "ghcr.io/elias969/gyn.git"
+      docker_registry_url = "https://ghcr.io"
+    }
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  https_only = true
+}
